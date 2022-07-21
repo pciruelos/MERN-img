6073b6530ed7f5394ebd7bca97bb713d17577aee
pciruelos <pciruelos@yahoo.com>
2022-07-21 10:12:14 +0200

    3

--- server/app.js
+++ server/app.js
@@ -3,9 +3,12 @@
 import express from 'express'
 import fileUpload from 'express-fileupload'
 import postsRoutes from './routes/post.routes.js'
+import {dirname, join} from 'path'
+import {fileURLToPath} from 'url'
 
 //start express
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 //middlewares
 app.use(express.json())
@@ -16,5 +19,7 @@ app.use(fileUpload({
 
 // routes
 app.use(postsRoutes)
+
+app.use(express.static(join(__dirname,'../client/build')))
 
 export default app
